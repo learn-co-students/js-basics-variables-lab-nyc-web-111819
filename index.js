186c174cb63d1c0b00da0abbe9a2045ad44cde9a
Your Name <samanbatool08@gmail.com>
2020-01-05 13:57:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+let companyName = 'Scuber';
+
+const companyName = 'Scuber';
+
+var mostProfitableNeighborhood = 'Chelsea';
+
+mostProfitableNeighborhood = 'Upper West Side'
+
+let companyCeo = 'Susan Smith'
+
+companyCeo = 'Lauren Hart'
